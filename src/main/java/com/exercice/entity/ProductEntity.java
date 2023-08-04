b69da0fb3ff977ba1b225d3c57c12224832b5950
CestMoiChrist <svay.christloyd@gmail.com>
2023-08-04 11:51:13 +0200

    4eme commit

--- src/main/java/com/exercice/entity/ProductEntity.java
+++ src/main/java/com/exercice/entity/ProductEntity.java
@@ -1,72 +1,84 @@
 package com.exercice.entity;
 
+/**
+ * Represents a Product entity. <br/>
+ * This class extends the AbstractEntity class and implements the IProductEntity interface.
+ * It represents a product with properties such as name, price, and product ID.
+ */
 public class ProductEntity extends AbstractEntity implements IProductEntity {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String name;
-	private Double price;
-	
-	private Integer productId;
-	
-	public ProductEntity() {
-		super();
-	}
-	
-	
-	public ProductEntity(Integer id, String name, Double price) {
-		super(id);
-		this.setName(name);
-		this.setPrice(price);
-	}
-	
-
-	@Override
-	public Integer getProductId() {
-		return this.productId;
-	}
-
-	@Override
-	public void setProductId(Integer pProductId) {
-		this.productId = pProductId;
-	}
-
-
-	@Override
-	public String getName() {
-		return name;
-	}
-
-
-	@Override
-	public void setName(String name) {
-		if((name == null) || (name.trim().length() == 0)) {
-			name = null;
-		} else {
-			this.name = name;
-		}
-	}
-
-
-	@Override
-	public Double getPrice() {
-		return price;
-	}
-
-
-	@Override
-	public void setPrice(Double price) {
-		if(price >= 0 ) {
-			this.price = price;
-		}
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "ProductEntity [id=" + id + ", name=" + name + ", price=" + price + "]";
-	}
-	
-	
+    
+    private static final long serialVersionUID = 1L;
+    
+    // Properties specific to ProductEntity
+    private String name;
+    private Double price;
+    private Integer productId;
+    
+    /**
+     * Default constructor for ProductEntity.
+     * Initializes the properties with default values.
+     */
+    public ProductEntity() {
+        super();
+    }
+    
+    /**
+     * Constructor for ProductEntity with parameters.
+     * Initializes the ID, name, and price of the product.
+     *
+     * @param id    the ID of the product
+     * @param name  the name of the product
+     * @param price the price of the product
+     */
+    public ProductEntity(Integer id, String name, Double price) {
+        super(id);
+        this.setName(name);
+        this.setPrice(price);
+    }
+    
+    // Implementation of methods from IProductEntity interface
+    
+    @Override
+    public Integer getProductId() {
+        return this.productId;
+    }
+    
+    @Override
+    public void setProductId(Integer pProductId) {
+        this.productId = pProductId;
+    }
+    
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    @Override
+    public void setName(String name) {
+        // Set the name if it is not empty or null, otherwise set it to null.
+        if ((name == null) || (name.trim().length() == 0)) {
+            name = null;
+        } else {
+            this.name = name;
+        }
+    }
+    
+    @Override
+    public Double getPrice() {
+        return price;
+    }
+    
+    @Override
+    public void setPrice(Double price) {
+        // Set the price if it is non-negative, otherwise do not change the value.
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return "ProductEntity [id=" + id + ", name=" + name + ", price=" + price + "]";
+    }
+    
 }
--- src/test/java/root/CartNatixisApplicationTests.java
+++ src/test/java/root/CartNatixisApplicationTests.java
@@ -1,13 +1,27 @@
 package root;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.exercice.http.Cart;
+
 @SpringBootTest
 class CartNatixisApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
+    public void testGetTotal() {
+        Cart cart = new Cart(1, "Product A", 10.0);
+        cart.setQuantity(2);
+        double expectedTotal = 20.0;
+        assertEquals(expectedTotal, cart.getTotal(), 0.01, "Incorrect total price calculation");
+    }
 
+    @Test
+    public void testToString() {
+        Cart cart = new Cart(2, "Product B", 5.5);
+        cart.setQuantity(3);
+        String expectedString = "Cart [id=2, name=Product B, quantity=3, price=5.5, total=16.5]";
+        assertEquals(expectedString, cart.toString(), "Incorrect toString representation");
+    }
 }
